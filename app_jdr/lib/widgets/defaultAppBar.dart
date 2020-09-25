24fa323485caa47aa6790ecc48aff91ab64f51e2
EtienneDuv <etienne.duverney@viacesi.fr>
2020-09-25 10:47:28 +0200

    appBar

--- app_jdr/lib/widgets/defaultAppBar.dart
+++ app_jdr/lib/widgets/defaultAppBar.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
-import 'dart:developer';
 
 Widget defaultAppBar(String title, List<Widget> icons, BuildContext context) {
   final isRoot = ModalRoute.of(context).settings.name == '/' ? true : false;
   return AppBar(
     leading: !isRoot
-        ? IconButton(icon: Icon(Icons.arrow_back), onPressed: () {})
+        ? IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+            })
         : null,
     title: Text(title),
     actions: icons,
