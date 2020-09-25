0bb57c356477b5c4f8af16421437bad076599872
EtienneDuv <etienne.duverney@viacesi.fr>
2020-09-25 17:18:37 +0200

    dmInterface list

--- app_jdr/lib/screens/dmInterface/dmInterface.dart
+++ app_jdr/lib/screens/dmInterface/dmInterface.dart
@@ -6,7 +6,37 @@ class DmInterface extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-      appBar: defaultAppBar('Dm Interface', [], context),
-    ));
+            appBar: defaultAppBar('Dm Interface', [], context),
+            body: ListView(
+              children: [
+                Center(
+                  child: Container(
+                    height: 50,
+                    width: MediaQuery.of(context).size.width * 0.70,
+                    color: Colors.black12,
+                    margin: EdgeInsets.symmetric(vertical: 20),
+                    child: Center(child: Text("Dm Interface")),
+                  ),
+                ),
+                Center(
+                  child: Container(
+                    height: 50,
+                    width: MediaQuery.of(context).size.width * 0.70,
+                    color: Colors.black12,
+                    margin: EdgeInsets.symmetric(vertical: 20),
+                    child: Center(child: Text("Dm Interface")),
+                  ),
+                ),
+                Center(
+                  child: Container(
+                    height: 50,
+                    width: MediaQuery.of(context).size.width * 0.70,
+                    color: Colors.black12,
+                    margin: EdgeInsets.symmetric(vertical: 20),
+                    child: Center(child: Text("Dm Interface")),
+                  ),
+                )
+              ],
+            )));
   }
 }
